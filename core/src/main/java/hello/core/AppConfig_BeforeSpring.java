607ee481d33a695f5f70c6295c516c849fa90cdc
Hchan <hchani95@gamil.com>
2023-06-29 22:06:08 +0900

    스프링으로 전환

--- core/src/main/java/hello/core/AppConfig_BeforeSpring.java
+++ core/src/main/java/hello/core/AppConfig_BeforeSpring.java
@@ -1,33 +1,28 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class AppConfig {
+// 실제 애플리케이션의 동작에 필요한 "구현 객체를 생성" 한다!
+// AppConfig 는 생성한 객체 인스턴스의 참조(레퍼런스)를 "생성자를 통해서 주입(연결)" 한다.
+// 객체의 생성과 연결은 AppConfig가 담당
+// DIP 완성!
+public class AppConfig_BeforeSpring {
 
-    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
-
-    @Bean
-    public MemoryMemberRepository memberRepository() {
+    private MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
